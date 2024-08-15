944fcb11225ff89e8af340ae6332926f44ff47cd
KaranBhullar <cswailord@gmail.com>
2024-08-15 16:01:55 -0400

    basic modal implementation

---
+++ frontend/src/components/OptionContent.vue
@@ -0,0 +1,70 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const picked = ref('1')
+</script>
+
+<template>
+  <div class="float-left menu  p-0 [&_li>*]:rounded-none bg-base-200">
+    <ul class="  h-full   w-max">
+      <li>
+        <label>
+          Theme
+          <input class="hidden" type="radio" name="option" value="1" v-model="picked"/>
+        </label>
+      </li>
+      <li>
+        <label>
+          Sound
+          <input class="hidden" type="radio" name="option" value="2" v-model="picked"/>
+        </label>
+      </li>
+      <li>
+        <label>
+          TEXT
+          <input class="hidden" type="radio"  name="option" value="3" v-model="picked"/>
+        </label>
+      </li>
+    </ul>
+
+  </div>
+  <div v-if="picked == 1" class="flex flex-col p-2 m-2">
+    <div class="form-control">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Default</span>
+        <input type="radio" name="theme-radios" class="radio theme-controller" value="default" />
+      </label>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Retro</span>
+        <input type="radio" name="theme-radios" class="radio theme-controller" value="retro" />
+      </label>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Cyberpunk</span>
+        <input type="radio" name="theme-radios" class="radio theme-controller" value="cyberpunk" />
+      </label>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Valentine</span>
+        <input type="radio" name="theme-radios" class="radio theme-controller" value="valentine" />
+      </label>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer gap-4">
+        <span class="label-text">Aqua</span>
+        <input type="radio" name="theme-radios" class="radio theme-controller" value="aqua" />
+      </label>
+    </div>
+
+  </div>
+  <div v-if="picked == 2">
+    
+  </div>
+  <div v-if="picked == 3">
+
+  </div>
+</template>
--- frontend/src/templates/Home.vue
+++ frontend/src/templates/Home.vue
@@ -1,25 +1,30 @@
 <script setup>
   import { ref } from 'vue';
-  import Render from '../components/Render.vue';
-  let t = ref(false);
+  import JoinOptions from '../components/JoinGames.vue';
+  import Settings from '../components/Settings.vue';
+  let toggle = ref(false);
 
-  const flip = () => {t.value = !t.value}
+  const flip = () => {toggle.value = !toggle.value}
 </script>
 
 <template>
  
-  <div class="w-full">
+  <div class="w-full h-full">
+    <div class="float-right m-4">
+      <Settings /> 
+    </div>
+   
     <div class="w-1/3 float-left">
-        <div class="h-screen flex justify-center items-center">
+        <div class="  flex justify-center">
             <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"> NEEDS TO BE IMPLEMENTED</button>
         </div>
     </div>
     <div class=" w-2/3 float-right"> 
-      <div class="h-screen flex justify-center items-center">
-        <div v-show="t">
-          <Render t="t" @te="flip"/>
+      <div class="flex justify-center items-center">
+        <div v-show="toggle">
+          <JoinOptions toggle="toggle" @flip="flip"/>
         </div>
-        <div v-show="!t" class="flex flex-col">
+        <div v-show="!toggle" class="flex flex-col">
           <button @click="flip" id="Join" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Join Game</button>
           <div class="divider">or</div>
           <button id="Create" class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Create Game</button>
